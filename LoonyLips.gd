a57d9a67caeaec274d850865ac53888c09bbd58d
Marco Santos <marcongsantos@gmail.com>
2019-09-01 18:51:21 +0800

    Lecture 15 Completed - Code Base
    Full code base from Lecture 15

---
+++ LoonyLips.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _ready():
+
+	var prompts =["Yann", "Minions", "greatest"]
+	var story = "Once upon a time %s watched %s and thought it was the %s movie ever"
+	$VBoxContainer/DisplayText.text = story % prompts
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func _on_TextureButton_pressed():
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayText(words)
+
+	
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
