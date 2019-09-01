78968c3541f3b5e249d96aecdfe7e2415188791d
Marco Santos <marcongsantos@gmail.com>
2019-09-01 20:06:05 +0800

    Update LoonyLips.gd
    Lecture 16 - Complete

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,21 +1,51 @@
 extends Control
 
+var player_words = []
+var prompts = ["a name", "a noun", "adverb", "adjective"]
+var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him all %s inside of %s"
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
+
 func _ready():
-
-	var prompts =["Yann", "Minions", "greatest"]
-	var story = "Once upon a time %s watched %s and thought it was the %s movie ever"
-	$VBoxContainer/DisplayText.text = story % prompts
-
+	DisplayText.text = "Welcome to Loony Lips! We're going to tell a story and have a wonderful time! "
+	check_player_words_length()
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
-
+	add_to_player_words()
 
 func _on_TextureButton_pressed():
-	var words = $VBoxContainer/HBoxContainer/PlayerText.text
-	update_DisplayText(words)
+	add_to_player_words()
+	
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+	
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else: #catch all
+		prompt_player()
+
+		
+func tell_story():
+	DisplayText.text = story % player_words
 
 	
-func update_DisplayText(words):
-	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func prompt_player():
+	DisplayText.text += "May I Have " + prompts[player_words.size()] + " please?"
+
+
+
+
+
+
+
+
+
+	
